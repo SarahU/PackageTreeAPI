3237ca8421d4ff978ec13d5df056d6426256cb1e
Sarah Usher <sarahnusher@gmail.com>
2020-08-23 16:43:23 +0100

    Added dockerfile and run instructions for this (easier as I''m currently on Windows)

--- README.md
+++ README.md
@@ -1,16 +1,23 @@
 # PackageTreeAPI
 
 How to run:
-Command Line:
+Command Line using docker:
+docker build -t packagetreeapi .
 
-Tests:
+`docker run --rm -ti -p 8080:8080 packagetreeapi`
+
+## Decisions:
+
+For version processing, where there is a choice, the more recent version was selected.
+Performing API calls per set of dependencies. This seems to scale well enough and was less complex than
+a solution with a central map of packages and versions that gets updated via queued API calls. But is also
+more susceptible to a scale issue if the set of dependencies for one package is very large.
 
 
-To Do:
+## To Do:
 Error Handling for failed NPM API call
 More processing for versions
 
 
-Queries:
-How to handle multiple versions in dependency list?
-Return as a file?
+## Queries:
+How to handle multiple versions in dependency list ie do we want to see this?
--- src/main/java/com/sarahu/packageapp/PackageRetriever.java
+++ src/main/java/com/sarahu/packageapp/PackageRetriever.java
@@ -17,13 +17,11 @@ import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
 public class PackageRetriever {
-    String NPM_BASE_URL = "https://registry.npmjs.org";
-    HttpClient client = null;
-    ExecutorService executorService = Executors.newFixedThreadPool(20);
-//    PackagesRepository repository = new PackagesRepository();
-    ObjectMapper objectMapper = new ObjectMapper();
+    private final ObjectMapper objectMapper = new ObjectMapper();
+    private HttpClient client = null;
 
     public PackageRetriever(){
+        ExecutorService executorService = Executors.newFixedThreadPool(20);
         client = HttpClient.newBuilder()
                 .executor(executorService)
                 .build();
@@ -31,9 +29,9 @@ public class PackageRetriever {
     
     public PackageData RetrievePackageDataFromAPI(String PackageName, String Version) {
         try {
-            var root = retrievePackageFromAPIAsync(PackageName, Version).join();
+            var root = buildAsyncRequest(PackageName, Version).join();
             var parsedRoot = parsePackageInfo(root, PackageName,Version);
-            getDependencyPackageData(parsedRoot);
+            processDependencyData(parsedRoot);
             return parsedRoot;
         }catch (Throwable e){
             System.out.println(e);
@@ -41,37 +39,19 @@ public class PackageRetriever {
         }
     }
 
-    private HttpRequest createRequest(String url) {
-        return HttpRequest.newBuilder()
+    private CompletableFuture<String> buildAsyncRequest(String PackageName, String Version){
+        System.out.println("Processing:" + PackageName + "::" + Version);
+        String NPM_BASE_URL = "https://registry.npmjs.org";
+        String url = NPM_BASE_URL + "/" + PackageName + "/" + Version;
+
+        HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(url))
                 .header("Content-Type", "application/json")
                 .build();
-    }
 
-    private CompletableFuture<String> retrievePackageFromAPIAsync(String PackageName, String Version){
-        System.out.println("Processing:" + PackageName + "::" + Version);
-        String url = NPM_BASE_URL + "/" + PackageName + "/" + Version;
-
-        HttpRequest request = createRequest(url);
-        return fetchPackageFromAPI(client, request);
-    }
-
-//    private CompletableFuture<PackageData> parseResponseAsync(Function<String, PackageData> parseFunction, HttpClient client, HttpRequest request) {
-//            return client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
-//                    .thenApply(HttpResponse::body)
-//                    .thenApply(parseFunction);
-//    }
-
-    private CompletableFuture<String> fetchPackageFromAPI(HttpClient client, HttpRequest request) {
         return client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
                 .thenApply(HttpResponse::body);
     }
-
-//    private Function<String, PackageData> parsePackageFromResponseData(PackageData data) {
-//        return item -> {
-//            return parsePackageInfo(item, data.getName(), data.getVersion());
-//        };
-//    }
 
     private PackageData parsePackageInfo(String item, String name, String version){
         JsonNode parser = null;
@@ -98,30 +78,23 @@ public class PackageRetriever {
         return dependencies;
     }
 
-    private void getDependencyPackageData(PackageData node) {
-        var futures = node.getDependencies().stream()
-                .map(i -> retrievePackageFromAPIAsync(i.getName(), i.getVersion()));
-
-        var deps = futures
+    private void processDependencyData(PackageData node) {
+        var parsedDependencies = node.getDependencies().stream()
+                .map(i -> buildAsyncRequest(i.getName(), i.getVersion()))
                 .map(CompletableFuture::join)
-                .parallel()
+                .parallel() //do not parallelize before this as  IO does not scale over fork/join architecture
                 .map(i -> parsePackageInfo(i, node.getName(), node.getVersion()))
                 .collect(Collectors.toList());
 
-        node.setDependencies(deps);
-        if(deps.stream().anyMatch(PackageData::hasDependencies)){
-            //recurse
-            deps.stream().filter(PackageData::hasDependencies)
-                    .forEach(this::getDependencyPackageData);
+        node.setDependencies(parsedDependencies);
+        if(parsedDependencies.stream().anyMatch(PackageData::hasDependencies)){
+            parsedDependencies.stream().filter(PackageData::hasDependencies)
+                                       .forEach(this::processDependencyData);
         }
     }
 
-
-    public static <T> Stream<T> getStreamFromIterator(Iterator<T> iterator) {
-        Spliterator<T>
-                spliterator = Spliterators
-                .spliteratorUnknownSize(iterator, 0);
-
+    private static <T> Stream<T> getStreamFromIterator(Iterator<T> iterator) {
+        Spliterator<T> spliterator = Spliterators.spliteratorUnknownSize(iterator, 0);
         return StreamSupport.stream(spliterator, true);
     }
 }
